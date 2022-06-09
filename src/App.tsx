6a5bf9b89263e9d9ec66354116d88eb6c42ded94
Eric Thomas <bluebird6922@gmail.com>
2022-06-09 16:43:05 +0300

    lineTraslate, lineHeight for overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import React, { useEffect, useRef } from "react";
 import "./App.css";
 
-export interface MainSceneProp {
+export interface Settings {
   logoOpacity: string;
   leavesScale: number;
+  overlayOpacity: number;
+  lineTranslate: number;
+  lineHeight: number;
 }
 
-const MainScene: React.FC<MainSceneProp> = (props) => {
+export interface MainSceneProp {
+  settings: Settings;
+}
+
+const MainScene: React.FC<MainSceneProp> = ({ settings }: MainSceneProp) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const bannerRef = useRef<HTMLImageElement>(null); //t
 
   useEffect(() => {
     if (containerRef.current) {
-      containerRef.current.style.opacity = props.logoOpacity;
+      containerRef.current.style.opacity = settings.logoOpacity;
     }
     if (bannerRef.current) {
       bannerRef.current.style.transform = `scale(${
-        1 + (props.leavesScale - 1) / 4
+        1 + (settings.leavesScale - 1) / 4
       })`;
     }
     /*if (d.current) {
       const translate = -(100 * settings.leavesScale - 100);
       d.current.style.transform = `scale(${settings.leavesScale}) translate3d(0, ${translate}px, 0)`;
     }*/
-  }, [props]);
+  }, [settings]);
 
   return (
     <div className="banner-container" ref={containerRef}>
@@ -48,13 +55,30 @@ const MainScene: React.FC<MainSceneProp> = (props) => {
   );
 };
 
+const settings = {
+  logoOpacity: "1",
+  leavesScale: 1,
+  overlayOpacity: -1,
+  lineTranslate: 0,
+  lineHeight: 0,
+} as Settings;
+
 function App() {
   return (
     <div>
       <div className="dik-banner-2d">
-        <MainScene logoOpacity='1' leavesScale={1} />
-        <div className="banner-overlay"></div>
-        <div className="scroll-line "></div>
+        <MainScene settings={settings} />
+        <div
+          className="banner-overlay"
+          style={{ opacity: settings.overlayOpacity }}
+        />
+        <div
+          className="scroll-line "
+          style={{
+            transform: `translate3d(-50%, ${-settings.lineTranslate}px, 0)`,
+            height: `${200 * settings.lineHeight}px`,
+          }}
+        />
       </div>
     </div>
   );
